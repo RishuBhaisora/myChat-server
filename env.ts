b3292d85f5f641f32950693579cf74e0ad87e119
RishuBhaisora <rdxrishabh5@gmail.com>
2023-04-19 20:48:22 +0530

    Added mail logic and functionality

--- env.ts
+++ env.ts
@@ -12,19 +12,23 @@
 |
 */
 
-import Env from '@ioc:Adonis/Core/Env'
+import Env from "@ioc:Adonis/Core/Env";
 
 export default Env.rules({
-	HOST: Env.schema.string({ format: 'host' }),
-	PORT: Env.schema.number(),
-	APP_KEY: Env.schema.string(),
-	APP_NAME: Env.schema.string(),
-    DRIVE_DISK: Env.schema.enum(['local'] as const),
-	NODE_ENV: Env.schema.enum(['development', 'production', 'test'] as const),
-	PG_HOST: Env.schema.string({ format: 'host' }),
-    PG_PORT: Env.schema.number(),
-	PG_USER: Env.schema.string(),
-	PG_PASSWORD: Env.schema.string.optional(),
-	PG_DB_NAME: Env.schema.string(),
-	DATABASE_URL:Env.schema.string.optional(),
-})
+  HOST: Env.schema.string({ format: "host" }),
+  PORT: Env.schema.number(),
+  APP_KEY: Env.schema.string(),
+  APP_NAME: Env.schema.string(),
+  DRIVE_DISK: Env.schema.enum(["local"] as const),
+  NODE_ENV: Env.schema.enum(["development", "production", "test"] as const),
+  PG_HOST: Env.schema.string({ format: "host" }),
+  PG_PORT: Env.schema.number(),
+  PG_USER: Env.schema.string(),
+  PG_PASSWORD: Env.schema.string.optional(),
+  PG_DB_NAME: Env.schema.string(),
+  DATABASE_URL: Env.schema.string.optional(),
+  SMTP_HOST: Env.schema.string({ format: "host" }),
+  SMTP_PORT: Env.schema.number(),
+  SMTP_USERNAME: Env.schema.string(),
+  SMTP_PASSWORD: Env.schema.string(),
+});
